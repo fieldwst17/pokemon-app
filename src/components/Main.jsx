42ad9711e1cb527d513b9a1ab470027e7776a8ed
fieldwst17 <149990079+fieldwst17@users.noreply.github.com>
2024-01-17 11:02:26 +0700

    components

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <>
+      <div className="container">
+          <div className="left-content">
+
+            </div>  
+          <div className="right-content">
+
+            </div>  
+      </div>
+    </>
+  )
+}
+
+export default Main
